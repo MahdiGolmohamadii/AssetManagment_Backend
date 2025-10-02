8c00a3c124739cc61f4f6dba1dd3e498225f85b8
MahdiGolmohamadii <mmahdigolmohamadi@gmail.com>
2025-10-02 13:56:46 -0400

    asset tests continues

--- app/test/test_assets.py
+++ app/test/test_assets.py
@@ -4,7 +4,7 @@ import pytest_asyncio
 
 from app.core import security
 from app.models.user import User
-from app.schemas.asset import AssetNew, AssetVersionNew, AssetUpdate
+from app.schemas.asset import AssetNew, AssetVersionNew, AssetUpdate, AssetVersionUpdate, AssetSearch
 from app.test.test_config import async_client, clean_db, get_test_session
 
 
@@ -13,6 +13,8 @@ ARTUSER ={"username":"art", "password":"art", "roles": "artist", "id":2}
 
 
 ASSET1 = {"name":"asset1", "description":"oh describe it","asset_type":"model", "id": 0, "versions":[]}
+ASSET2 = {"name":"asset2", "description":"oh describe it2","asset_type":"texture", "id": 0, "versions":[]}
+
 Ver1 = {"version_number": 1, "file_path": "/somewhere/imp", "status": "Published","id":0,  "asset_id": 0}
 ver2 = {"version_number": 2, "file_path": "/somewhere/imp2", "status": "ReadyForReview","id":0,  "asset_id": 0}
 ver3 = {"version_number": 3, "file_path": "/somewhere/imp3", "status": "Published","id":0,  "asset_id": 0}
@@ -54,6 +56,19 @@ async def test_add_new_asset(async_client, create_admin, admin_token):
     assert "id" in data
     ASSET1["id"] = data["id"]
 
+    payload = AssetNew(name=ASSET2["name"], description=ASSET2["description"], asset_type=ASSET2["asset_type"]).model_dump()
+    response = await async_client.post("/assets", headers=headers, json=payload)
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data["name"] == payload["name"]
+    assert data["description"] == payload["description"]
+    assert data["asset_type"] == payload["asset_type"]
+
+    assert "id" in data
+    ASSET2["id"] = data["id"]
+
 @pytest.mark.asyncio
 async def test_add_version(async_client, admin_token):
     headers = {"Authorization": f"Bearer {admin_token}"}
@@ -63,6 +78,18 @@ async def test_add_version(async_client, admin_token):
     assert response.status_code == 200
     response_data = response.json()
     assert response_data["asset_id"] == ASSET1["id"]
+    Ver1["id"] = response_data['id']
+    Ver1["asset_id"] = response_data['asset_id']
+    ASSET1['versions'].append(response_data)
+
+    payload = AssetVersionNew(version_number=ver2["version_number"], file_path=ver2["file_path"], status=ver2["status"]).model_dump()
+    response = await async_client.post(f"/assets/{ASSET1["id"]}", headers = headers, json=payload)
+
+    assert response.status_code == 200
+    response_data = response.json()
+    assert response_data["asset_id"] == ASSET1["id"]
+    ver2["id"] = response_data['id']
+    ver2["asset_id"] = response_data['asset_id']
     ASSET1['versions'].append(response_data)
 
     #TODO the following is fucked I have no idea why!
@@ -71,7 +98,7 @@ async def test_add_version(async_client, admin_token):
     # print(response.json())
 
 @pytest.mark.asyncio
-async def test_get_assete(async_client, admin_token):
+async def test_get_asset(async_client, admin_token):
     headers = {"Authorization": f"Bearer {admin_token}"}
     response = await async_client.get(f"/assets/{ASSET1['id']}", headers=headers)
     assert response.status_code == 200
@@ -100,3 +127,71 @@ async def test_update_asset(async_client, admin_token):
     response = await async_client.put(f"/assets/{100}", headers=headers, json=payload)
     assert response.status_code == 404
 
+
+@pytest.mark.asyncio
+async def test_get_asset_version(async_client, admin_token):
+    headers = {"Authorization": f"Bearer {admin_token}"}
+    reponse = await async_client.get(f"/assets/{ASSET1['id']}/{Ver1['id']}", headers=headers)
+    assert reponse.status_code == 200
+    response_data = reponse.json()
+    assert response_data['asset_id'] == ASSET1["id"]
+    assert response_data['id'] == Ver1["id"]
+
+    reponse = await async_client.get(f"/assets/{100}/{Ver1['id']}", headers=headers)
+    assert reponse.status_code == 404
+    assert reponse.json() == {'detail': 'Asset Or Version Not Found!'}
+
+    reponse = await async_client.get(f"/assets/{ASSET1['id']}/{500}", headers=headers)
+    assert reponse.status_code == 404
+    assert reponse.json() == {'detail': 'Asset Or Version Not Found!'}
+
+
+
+
+@pytest.mark.asyncio
+async def test_update_asset_version(async_client, admin_token):
+    headers = {"Authorization": f"Bearer {admin_token}"}
+    payload = AssetVersionUpdate(status="ReadyForReview").model_dump(exclude_unset=True)
+    response = await async_client.put(f"/assets/{ASSET1['id']}/{Ver1['id']}", headers=headers, json=payload)
+    assert response.status_code == 200
+    response_data = response.json()
+    assert response_data['status'] == "ReadyForReview"
+    Ver1["status"] = "ReadyForReview"
+    ASSET1["versions"][0] = Ver1
+
+    response = await async_client.put(f"/assets/{ASSET1['id']}/{200}", headers=headers, json=payload)
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Asset Not Found'}
+
+    response = await async_client.put(f"/assets/{123}/{Ver1['id']}", headers=headers, json=payload)
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Asset Not Found'}
+
+@pytest.mark.asyncio
+async def test_search_asset(async_client, admin_token):
+    headers = {"Authorization": f"Bearer {admin_token}"}
+    payload = AssetSearch().model_dump()
+    response = await async_client.get("/assets/", headers=headers)
+    assert response.status_code == 200
+    assert response.json() == [ASSET1, ASSET2]
+
+@pytest.mark.asyncio
+async def test_delete_version(async_client, admin_token):
+    headers = {"Authorization": f"Bearer {admin_token}"}
+    response = await async_client.delete(f"/assets/{ASSET1['id']}/{Ver1['id']}", headers=headers)
+    assert response.status_code == 200
+
+    response = await async_client.get(f"assets/{ASSET1['id']}/{Ver1['id']}", headers=headers)
+    assert response.status_code == 404
+
+@pytest.mark.asyncio
+async def test_delete_asset_version(async_client, admin_token):
+    headers = {"Authorization": f"Bearer {admin_token}"}
+    response = await async_client.delete(f"/assets/{ASSET1['id']}", headers=headers)
+    assert response.status_code == 200
+
+    response = await async_client.get(f"/assets/{ASSET1['id']}", headers=headers)
+    assert response.status_code == 404
+
+    response = await async_client.get(f"assets/{ASSET1}/{Ver1['id']}")
+    assert response.status_code == 404
